9bda0140763d021597258ecb86f000de0ca5d13d
bartoszlomiej <jet.samb@gmail.com>
2021-10-05 13:05:01 +0200

    Robots start moving after given time -> preparation to use the 'smart' timer in the clusters

--- 3S.py
+++ 3S.py
@@ -35,13 +35,14 @@ HORRIBLE_YELLOW = (190, 175, 50)
 
 BACKGROUND = WHITE
 
+
 class Robot(pg.sprite.Sprite):
     '''
     The object of the class Robot represents the robot on a board.
     
     It inherit from the base class sprite as this class is a basic representation of a character in pygame.
     '''
-    def __init__(self, x, y, width, height, velocity = [0, 0]):
+    def __init__(self, x, y, width, height, velocity=[0, 0]):
         super().__init__()
         '''
         Creates a robot on board with the initial coordinates (x, y) and the given velocity.
@@ -53,26 +54,27 @@ class Robot(pg.sprite.Sprite):
         self.height = height
         self.neighbors = []
         self.iterator = 0
-        self.messages = [] #obtained messages from other robots
-        self.broadcast = {} #messages to be broadcast
+        self.messages = []  #obtained messages from other robots
+        self.broadcast = {}  #messages to be broadcast
         '''
         Broadcast messages are the dictionary -> {msg_type : msg_value}
         Messages are the list of gather broadcasts from the neighbors
         It is assumed that it doesn't matter which robot place the massage
         '''
         self.AS = None
-        
+
         self.image = pg.Surface([self.radius * 2, self.radius * 2])
         self.image.fill(BACKGROUND)
-        
-        pg.draw.circle(self.image, GREEN, (self.radius, self.radius), self.radius)
-        
+
+        pg.draw.circle(self.image, GREEN, (self.radius, self.radius),
+                       self.radius)
+
         self.rect = self.image.get_rect()
         self.position = np.array([x, y], dtype=np.float64)
         self.velocity = np.asarray(velocity, dtype=np.float64)
         self.moved = False
 
-        self.state = "moving" #initially robots move (just for aggregation algorithm)
+        self.state = "moving"  #initially robots move (just for aggregation algorithm)
 
     def update(self):
         '''
@@ -86,14 +88,15 @@ class Robot(pg.sprite.Sprite):
         #place for the swarm behaviors
         self.aggregate()
         self.autonomus_system()
-        
+
         #boundary parameters
         if x < 0 or x > self.width - 2 * self.radius:
             self.velocity[0] = -self.velocity[0]
-        if y < 0 or y > self.height - 2 *self.radius:
+        if y < 0 or y > self.height - 2 * self.radius:
             self.velocity[1] = -self.velocity[1]
-            
-        self.neighbors.clear() #list of neighbors must be refreshed in each update
+
+        self.neighbors.clear(
+        )  #list of neighbors must be refreshed in each update
 
         self.rect.x = x
         self.rect.y = y
@@ -103,35 +106,50 @@ class Robot(pg.sprite.Sprite):
         Adds the newly spotted neighbor to the list.
         '''
         self.neighbors.append(r)
-            
+
     def aggregate(self):
         '''
         Simple aggregation behavior
         '''
-        if len(self.neighbors) == 0 and self.state != "moving":
-            velocity = np.random.rand(2)
-            velocity[0] = (velocity[0] - 0.5) * 4
-            velocity[1] = (velocity[1] - 0.5) * 4  #start moving
-        a =  (np.random.rand(1)/2) * (len(self.neighbors))
+        if len(self.neighbors
+               ) == 0 and self.state != "moving" and not self.velocity[
+                   0] and not self.velocity[1]:
+            '''
+            If there are no neighbors -> start moving
+            '''
+            self.velocity = np.random.rand(2)
+            self.velocity[0] = (self.velocity[0] - 0.5) * 4
+            self.velocity[1] = (self.velocity[1] - 0.5) * 4  #start moving
+        a = (np.random.rand(1) / 2) * (len(self.neighbors))
         p_coefficient = np.random.rand(1) * a * a
         if p_coefficient >= 0.75 and self.state == "moving":
+            '''
+            if there are some neighbors -> the robot might stop
+            '''
             self.state = "stopped"
             self.velocity[0] = 0
             self.velocity[1] = 0
-        if self.state == "stopped" and p_coefficient < 0.75:
-            velocity = np.random.rand(2)
-            velocity[0] = (velocity[0] - 0.5) * 4
-            velocity[1] = (velocity[1] - 0.5) * 4  #start moving
-#            self.state = "going to move"
-            self.state = "moving"
-        '''
-        if self.state == "going to move":
+        if self.state == "stopped" and p_coefficient < 0.8:
+            '''
+            if robot is stopped than there is possibility that robot will start moving
+            '''
             self.iterator = self.iterator + 1
-            if self.iterator > 500:
+            if self.iterator > 5000:
+                self.state = "moving after stopped"
+                self.iterator = 0
+                self.velocity = np.random.rand(2)
+                self.velocity[0] = (self.velocity[0] - 0.5) * 4
+                self.velocity[1] = (self.velocity[1] - 0.5) * 4  #start moving
+
+        elif self.state == "moving after stopped":
+            '''
+            if robot is moving after being stopped, than for some time it cannot stop again
+            '''
+            self.iterator = self.iterator + 1
+            if self.iterator > 50:
                 self.state = "moving"
                 self.iterator = 0
-                print("Am I here?")
-        '''
+
     def update_msg(self):
         self.messages.clear()
         for n in self.neighbors:
@@ -139,10 +157,12 @@ class Robot(pg.sprite.Sprite):
 
     def update_color(self):
         color = (self.AS, self.AS, self.AS)
-        pg.draw.circle(self.image, color, (self.radius, self.radius), self.radius)
-            
+        pg.draw.circle(self.image, color, (self.radius, self.radius),
+                       self.radius)
+
     def create_AS(self):
-        self.AS = np.random.randint(0, 255) #arbitrary number to be changed in the future
+        self.AS = np.random.randint(
+            0, 255)  #arbitrary number to be changed in the future
         self.broadcast['AS'] = self.AS
         self.update_color()
 
@@ -153,18 +173,18 @@ class Robot(pg.sprite.Sprite):
             if 'AS' in m.keys():
                 if not m['AS'] in votes:
                     votes[m['AS']] = 1
-                    if m['AS'] == self.AS: #don't change AS if you can still spot your colleagues
+                    if m['AS'] == self.AS:  #don't change AS if you can still spot your colleagues
                         remain = True
                 else:
                     votes[m['AS']] = votes[m['AS']] + 1
-        if not votes: #or remain:
+        if not votes:  #or remain:
             return None
-        k=list(votes.keys())
-        v=list(votes.values())
-        return k[v.index(max(v))] #getting the AS that is most common nearby
-            
+        k = list(votes.keys())
+        v = list(votes.values())
+        return k[v.index(max(v))]  #getting the AS that is most common nearby
+
     def autonomus_system(self):
-        self.broadcast.clear() #clearing broadcast
+        self.broadcast.clear()  #clearing broadcast
         '''
             Shows the idea of the autonomus system task allocation
             '''
@@ -175,13 +195,12 @@ class Robot(pg.sprite.Sprite):
             self.AS = new_AS
             self.broadcast['AS'] = new_AS
             self.update_color()
-        else: #preserving the old AS
+        else:  #preserving the old AS
             self.broadcast['AS'] = self.AS
-            
 
 
 class Simulation:
-    def __init__(self, width=640, height=400, N=10, s_range = 40):
+    def __init__(self, width=640, height=400, N=10, s_range=40):
         '''
         width - the width of the screen
         height - the height of the screen
@@ -191,8 +210,8 @@ class Simulation:
         self.width = width
         self.height = height
         self.size = (width, height)
-        self.sensor_range = s_range #(as 20 is the delimiter of the robot)
-        
+        self.sensor_range = s_range  #(as 20 is the delimiter of the robot)
+
         self.swarm = pg.sprite.Group()
         self.swarm_quantity = N
 
@@ -208,7 +227,6 @@ class Simulation:
             velocity = np.random.rand(2)
             velocity[0] = (velocity[0] - 0.5) * 4
             velocity[1] = (velocity[1] - 0.5) * 4
-
             '''
             TODO: Starting position could be checked here to avoid overlaping
             self.board[i] = [x, y]
@@ -221,15 +239,16 @@ class Simulation:
         For each robot in a swarm checks if the collision occurs. If so then the velocity is being changed accordingly.
         '''
         for robot in self.swarm:
-            collision_group = pg.sprite.Group([s for s in self.swarm if s != robot])
-            collide = pg.sprite.spritecollide(robot, collision_group, False) 
+            collision_group = pg.sprite.Group(
+                [s for s in self.swarm if s != robot])
+            collide = pg.sprite.spritecollide(robot, collision_group, False)
             if collide:
                 self.moved = True
-                for c in collide: #there can be numerous collisions however it is unlikely
+                for c in collide:  #there can be numerous collisions however it is unlikely
                     if c.moved == True:
                         continue
                     robot.velocity = [-robot.velocity[0], -robot.velocity[1]]
-                    
+
     def robot_vision(self):
         '''
         Emulates the very basic vision sensor of each robot in the swarm.
@@ -238,11 +257,11 @@ class Simulation:
             for i in self.swarm:
                 if r == i:
                     continue
-                if (abs(r.position[0] - i.position[0]) < self.sensor_range) and (
-                        abs(r.position[1] - i.position[1]) < self.sensor_range):
+                if (abs(r.position[0] - i.position[0]) < self.sensor_range
+                    ) and (abs(r.position[1] - i.position[1]) <
+                           self.sensor_range):
                     r.spotted(i)
-            
-        
+
     def run(self):
         '''
         Runs the simulation. After certain time the simulation is closed.
@@ -261,7 +280,7 @@ class Simulation:
             screen.fill(BACKGROUND)
             self.swarm.draw(screen)
             pg.display.flip()
-            clock.tick(30)
+            clock.tick(100)
         pg.quit()
 
 
@@ -298,7 +317,7 @@ class App:
             self.on_render()
         self.on_cleanup()
 '''
- 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     sim = Simulation(1024, 720, 25, 60)
     sim.run()
