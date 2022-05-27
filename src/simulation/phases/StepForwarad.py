9b467bd429a1d93c31991412581ea4d42aaab124
bartoszlomiej <jet.samb@gmail.com>
2022-05-27 15:38:09 +0200

    working on step forward

--- src/simulation/phases/StepForwarad.py
+++ src/simulation/phases/StepForwarad.py
@@ -2,10 +2,10 @@ from simulation.robot.Velocity import Velocity
 from simulation.phases.StaticLine import StaticLine
 from utils import SpotNeighbor as spot
 
-class CountToTwo(StaticLine):
+class StepForward(StaticLine):
     def __init__(self, Robot, superAS):
         super().__init__(Robot, superAS)
-        self.phase = 3.5
+        self.phase = 4
         self.isIncreased = False
 
     def __getSuperclustersMembers(self):        
@@ -22,7 +22,15 @@ class CountToTwo(StaticLine):
             if n.super_cluster_id == self.robot.super_cluster_id:
                 if not n.cluster_id in allowed:
                     allowed.append(n.cluster_id)
-        return allowed        
+        return allowed
+
+    def __getSameClusterMembers(self):
+        self.same_cluster_neighbors.clear()
+        for n in self.robot.neighbors:
+            if n.cluster_id != self.robot.cluster_id:
+                continue
+            self.same_cluster_neighbors.append(n)
+        return self.same_cluster_neighbors
 
     def __getClosestNeighborCluster(self):
         supercluster_members = self.__getSuperclustersMembersID()
@@ -32,34 +40,49 @@ class CountToTwo(StaticLine):
         else:
             return best_neighbor.cluster_id
 
-    def __setTimer(self):
-        self.robot.setTimer(20)
-        self.timerSet = True
-        
-    def __upgradeIfTimerFinished(self):
-        if self.robot.timer.duration < 0:
-            self.upgrade(4, self.robot.super_cluster_id)
-            
-    def __useTimerIfSet(self):
-        if not self.timerSet:
-            self.__setTimer()
+    def __checkForStepForward(self):
+        self.robot.follower_msg() #there is a need of direction
+        closest_neighbor = spot.find_best_neighbor(self.robot)
+        if not closest_neighbor:
+            return
+        self.__stepForwardIfHigherPriority(closest_neighbor)
+
+    def __stepForwardIfHigherPriority(self, closest_neighbor):
+        if self.__isHigherClusterID(closest_neighbor)
+            self.__stepOutFromLine(closest_neighbor)
         else:
-            self.robot.timer.tick()
-            self.__upgradeIfTimerFinished()
+            self.__goCloserToPreviousNeighbor()
+        self.__moveIfPathIsFree()
 
-    def __upgradeCluster(self):
-        self.robot.cluster_id += 100 
-        self.isIncreased = True
+    def __isHigherClusterID(self, closest_neighbor):
+        if closest_neighbor.cluster_id < self.robot.cluster_id:
+            return True
+        return False
 
-    def __downgradeCluster(self):
-        self.robot.cluster_id -= 100
-        self.isIncreased = False
+    def __stepOutFromLine(self, closest_neighbor):
+        if spot.is_follower(self.robot):
+            spot.follower(self.robot)
+            if self.robot.dir_x == 0 or self.robot.dir_y == 0:
+                self.__tryPerpendicularMotion(closest_neighbor)
+        else:
+            self.__tryPerpendicularMotion(closest_neighbor)
+        
+    def __tryPerpendicularMotion(self):
+        main_cluster_neighbors = self.getMainClusterNeighbors()
+        if not main_cluster_neighbors:
+            return
+        
+        best_neighbor = self._findClosestNeighbor(main_cluster_neighbors)
+        spot.direction_to_neighbor(self.robot, best_neighbor)
+        self.robot.direction.perpendicular()
+        
+    def __goCloserToPreviousNeighbor(self):
+        spot.follower(self.robot)
 
-    def __updateClusterID(self):
-            if not self.isIncreased:
-                self.__upgradeCluster()
-            else:
-                self.__downgradeCluster()        
+    def __moveIfPathIsFree(self):
+        if not spot.is_any_collision(self.robot, 0.15):
+            self.robot.direction.normalize()
+            self.makeMove()            
 
     def __isInSameCluster(self, previous_robot_cluster_id):
         if previous_robot_cluster_id == self.robot.cluster_id:
@@ -71,28 +94,20 @@ class CountToTwo(StaticLine):
             self.__updateClusterID()
         else:
             self.robot.update_color() #just for dbg
-    
-    def __countToTwo(self):
-        previous_robot_cluster_id = self.__getClosestNeighborCluster()
-        if not previous_robot_cluster_id: #only leader should not have a previous AS
-            self.__useTimerIfSet()
-            return
-        self.__changeClusterIfSame(previous_robot_cluster_id)
 
     def __keepStaticLine(self):
         self.same_cluster_neighbors.clear()
-        self.same_cluster_neighbors = self.__getSuperclustersMembers()
+        self.same_cluster_neighbors = self.__getSameClusterMembers()
         if self._isEdgeRobot():
             self.edgeRobotFunctionallity(0.5)
         else:
             self.insideRobotFunctionallity()
             
-        
     def update(self):
         self.check_phase()
-        self.__countToTwo()
         self.robot.velocity = Velocity(0, 0)
-        self.__keepStaticLine()
+        self.__checkForStepForward()
+        #self.__keepStaticLine()
         self.robot.isAlloneInSupercluster()
         
     def check_phase(self):
